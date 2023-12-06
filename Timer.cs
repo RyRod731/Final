28827036c30f22c5f635e157d7106b31b20f2fe8
Ryan Rodriguez <144503255+RyRod731@users.noreply.github.com>
2023-12-06 12:12:09 -0500

    Add files via upload

---
+++ Timer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeValue = 90;
+    public Text timerText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+        }
+        
+    }
+
+}
+
+
